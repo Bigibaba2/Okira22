4ccb5d8b92b897f59c926faec8a3f20264e65d7b
Bigibaba2 <arminbiigii2@gmail.com>
2026-03-28 16:48:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 REPO_OWNER="Bigibaba2"
 REPO_NAME="Okira22"
-RELEASE_TAG="v1.0.1-controlled-launch"
+RELEASE_TAG="v1.0.0-controlled-launch"
 PKG_VERSION="1.0.1-2"
 PKG_NAME="okira22_${PKG_VERSION}_all.deb"
 DOWNLOAD_URL="https://github.com/${REPO_OWNER}/${REPO_NAME}/releases/download/${RELEASE_TAG}/${PKG_NAME}"
